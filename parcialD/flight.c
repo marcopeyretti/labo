7b19d0825c85a51b356c6cb727308d6c8858fd28
marcopeyretti <166165529+marcopeyretti@users.noreply.github.com>
2024-05-03 03:16:41 -0300

    Add files via upload

---
+++ parcialD/flight.c
@@ -0,0 +1,27 @@
+/*
+  @file layover.c
+  @brief Implements flight structure and methods
+*/
+#include <stdlib.h>
+#include "flight.h"
+
+static const int AMOUNT_OF_FLIGHT_VARS = 3;
+
+Flight flight_from_file(FILE* file)
+{
+   Flight flight;
+  if(file!=NULL)
+  { 
+
+    int res=fscanf(file,EXPECTED_FLIGHT_FILE_FORMAT,& flight.hour,& flight.delay,& flight.passengers_amount);
+
+    if(res!=AMOUNT_OF_FLIGHT_VARS)
+    {
+      fprintf(stderr,"unexpected data\n");  
+      exit(EXIT_FAILURE);
+    }
+    
+  }
+
+    return flight;
+}
